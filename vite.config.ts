1ee672e7b79d545e36fc3e99d34276195b6834f7
donny <donny526@outlook.com>
2025-01-21 15:34:42 +0800

    chore: add react-compiler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import type { ConfigEnv } from 'vite'
+
+const ReactCompilerConfig = {}
 
 export default defineConfig(({ command }: ConfigEnv) => {
   const isBuild = command === 'build'
 
   console.log(isBuild)
   return {
-    plugins: [react(), tsconfigPaths(), unocss()],
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+        },
+      }),
+      tsconfigPaths(),
+      unocss(),
+    ],
   }
 })
